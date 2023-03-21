79c6c1a2f10dcd4491cc0b4671afd51f8219fe71
Pedro Prieto <pedpral@gmail.com>
2023-03-21 15:59:35 +0100

    Iconos en botones de documentación

--- src/components/DocumentacionList.vue
+++ src/components/DocumentacionList.vue
@@ -3,20 +3,40 @@
   <div class="panel-block">
     <div class="buttons">
       <button :disabled="!anySelected" @click="generarCertificados('cert_alumno')" class="button is-outlined is-primary">
-        Certificados de alumado
+        <span class="icon">
+          <svg-icon class="mdi" type="mdi" :path="mdiAccountSchool"></svg-icon>
+        </span>
+        <span>
+          Certificados de alumado
+        </span>
       </button>
       <button
         :disabled="!anySelected"
         @click="generarCertificados('cert_instructor')"
         class="button is-outlined is-primary"
         >
-        Certificados de instructores
+        <span class="icon">
+          <svg-icon class="mdi" type="mdi" :path="mdiHumanMaleBoard"></svg-icon>
+        </span>
+        <span>
+          Certificados de instructores
+        </span>
       </button>
       <button :disabled="!anySelected" @click="generarCertificados('fm18')" class="button is-outlined is-primary">
-        FM18s
+        <span class="icon">
+          <svg-icon class="mdi" type="mdi" :path="mdiClipboardListOutline"></svg-icon>
+        </span>
+        <span>
+          FM18s
+        </span>
       </button>
       <button :disabled="!anySelected" @click="mostrarEtiquetas()" class="button is-outlined is-primary">
-        Etiquetas
+        <span class="icon">
+          <svg-icon class="mdi" type="mdi" :path="mdiLabelOutline"></svg-icon>
+        </span>
+        <span>
+          Etiquetas
+        </span>
       </button>
     </div>
   </div>
@@ -35,11 +55,20 @@
 </template>
 
 <script>
+import SvgIcon from '@jamescoyle/vue-icon'
 import { gestionFCTStore } from '@/stores/gestionfct'
+import { mdiAccountSchool, mdiHumanMaleBoard, mdiClipboardListOutline, mdiLabelOutline } from '@mdi/js'
 
 export default {
+    components: {
+        SvgIcon
+    },
     data() {
         return {
+            mdiAccountSchool,
+            mdiHumanMaleBoard,
+            mdiLabelOutline,
+            mdiClipboardListOutline,
             allSelected: false,
             FCT: gestionFCTStore()
         }
