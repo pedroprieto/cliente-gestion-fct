65235d8b3e0f21f5f75ce85ef8cb3169f36175eb
Pedro Prieto <pedpral@gmail.com>
2023-03-22 14:34:11 +0100

    Arreglo fallo con this

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,12 +2,13 @@
   import { RouterLink } from 'vue-router'
   import  SelectorCursoPeriodo from './SelectorCursoPeriodo.vue'
   import { gestionFCTStore } from '@/stores/gestionfct'
-  import { useRouter } from 'vue-router'
+  import { useRouter, useRoute } from 'vue-router'
   import SvgIcon from '@jamescoyle/vue-icon'
   import { mdiLogoutVariant } from '@mdi/js'
   import { reactive } from 'vue'
 
   const router = useRouter();
+  const rutaActual = useRoute()
   const rutas = router.getRoutes().filter(route => (route.name != 'login') && (route.name!='anyadirVisita') && (route.name != 'editarVisita') && (route.name !='visitas') && (route.name !='etiquetas') && (route.name!='root'));
   const FCT = gestionFCTStore();
   const state = reactive({ navbar_activa: false })
@@ -37,7 +38,7 @@
       </div>
       <div :class="{'is-active': state.navbar_activa}" class="navbar-menu" id="navbar-app">
         <div class="navbar-start">
-          <RouterLink v-for="ruta of rutas" :key="ruta.id" :class="{'is-active': this.$route.name == ruta.name}" class="navbar-item" :to="{name: ruta.name, params: {curso: FCT.curso, periodo: FCT.periodo}}">{{ruta.meta.prompt}}</RouterLink>
+          <RouterLink v-for="ruta of rutas" :key="ruta.id" :class="{'is-active': rutaActual.name == ruta.name}" class="navbar-item" :to="{name: ruta.name, params: {curso: FCT.curso, periodo: FCT.periodo}}">{{ruta.meta.prompt}}</RouterLink>
         </div>
         <div class="navbar-end">
           <SelectorCursoPeriodo/>
