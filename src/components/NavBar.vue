724f6ddba47ee6cba5e6420709bdfda99601c61c
Pedro Prieto <pedpral@gmail.com>
2025-04-03 17:26:39 +0200

    Prettify

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,36 @@
 <script setup>
-  import { RouterLink } from 'vue-router'
-  import  SelectorCursoPeriodo from './SelectorCursoPeriodo.vue'
-  import { gestionFCTStore } from '@/stores/gestionfct'
-  import { useRouter, useRoute } from 'vue-router'
-  import SvgIcon from '@jamescoyle/vue-icon'
-  import { mdiLogoutVariant } from '@mdi/js'
-  import { reactive } from 'vue'
+import { RouterLink } from 'vue-router'
+import SelectorCursoPeriodo from './SelectorCursoPeriodo.vue'
+import { gestionFCTStore } from '@/stores/gestionfct'
+import { useRouter, useRoute } from 'vue-router'
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiLogoutVariant } from '@mdi/js'
+import { reactive } from 'vue'
 
-  const router = useRouter();
-  const rutaActual = useRoute()
-  const rutas = router.getRoutes().filter(route => (route.name != 'login') && (route.name!='anyadirVisita') && (route.name != 'editarVisita') && (route.name !='visitas') && (route.name !='etiquetas') && (route.name!='root'));
-  const FCT = gestionFCTStore();
-  const state = reactive({ navbar_activa: false })
+const router = useRouter()
+const rutaActual = useRoute()
+const rutas = router
+  .getRoutes()
+  .filter(
+    (route) =>
+      route.name != 'login' &&
+      route.name != 'anyadirVisita' &&
+      route.name != 'editarVisita' &&
+      route.name != 'visitas' &&
+      route.name != 'etiquetas' &&
+      route.name != 'root'
+  )
+const FCT = gestionFCTStore()
+const state = reactive({ navbar_activa: false })
 
-  function toggleNavbar() {
-  state.navbar_activa = !state.navbar_activa;
-  }
+function toggleNavbar() {
+  state.navbar_activa = !state.navbar_activa
+}
 
-  function logOut() {
-  FCT.deleteCredentials();
-  router.replace({name:'login'});
-  }
+function logOut() {
+  FCT.deleteCredentials()
+  router.replace({ name: 'login' })
+}
 </script>
 
 <template>
@@ -28,20 +38,35 @@
     <nav class="navbar is-primary has-shadow" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <!-- <a class="navbar-item"> -->
-          <!--   GestiónFCT -->
-          <!-- </a> -->
-        <a role="button" :class="{'is-active': state.navbar_activa}" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-app" @click="toggleNavbar()">
+        <!--   GestiónFCT -->
+        <!-- </a> -->
+        <a
+          role="button"
+          :class="{ 'is-active': state.navbar_activa }"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar-app"
+          @click="toggleNavbar()"
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div :class="{'is-active': state.navbar_activa}" class="navbar-menu" id="navbar-app">
+      <div :class="{ 'is-active': state.navbar_activa }" class="navbar-menu" id="navbar-app">
         <div class="navbar-start">
-          <RouterLink v-for="ruta of rutas" :key="ruta.id" :class="{'is-active': rutaActual.name == ruta.name}" class="navbar-item" :to="{name: ruta.name, params: {curso: FCT.curso, periodo: FCT.periodo}}">{{ruta.meta.prompt}}</RouterLink>
+          <RouterLink
+            v-for="ruta of rutas"
+            :key="ruta.id"
+            :class="{ 'is-active': rutaActual.name == ruta.name }"
+            class="navbar-item"
+            :to="{ name: ruta.name, params: { curso: FCT.curso, periodo: FCT.periodo } }"
+            >{{ ruta.meta.prompt }}</RouterLink
+          >
         </div>
         <div class="navbar-end">
-          <SelectorCursoPeriodo/>
+          <SelectorCursoPeriodo />
           <div class="navbar-item">
             <button @click="logOut()" class="button is-rounded is-fullwidth">
               <span class="icon">
